f3dee1017ed001ea76c13489dcc7e23e523622e8
Mark Robson <roppa_uk@hotmail.com>
2015-07-08 11:53:38 -0700

    Completed classes

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,9 @@
 var Bee = function() {
+  Grub.call(this);
+  this.age = 5;
+  this.color = "yellow";
+  this.job = "keep on growing";
 };
 
+Bee.prototype = new Grub();
+Bee.prototype.constructor = Bee;
